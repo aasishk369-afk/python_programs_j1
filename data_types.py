94a183f47ddeac9ff64fe97068db6835a3e80b93
aasishk369-afk <aasishk369@gmail.com>
2025-11-22 09:43:39 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#data types - in python 2 data types
+#1.int(integer)
+#2.str(string)
+#3.float(decimals)
+name="Aasish"
+age=15
+salary=10000.52
+data="1234"
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
